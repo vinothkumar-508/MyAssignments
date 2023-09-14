c9bc0ffc325802067734cbad5a24adfa185a319c
vinothkumar-508 <143920000+vinothkumar-508@users.noreply.github.com>
2023-09-14 19:48:28 +0530

    Add files via upload
    Day 2 Assignment

---
+++ day2/FindIntersection.java
@@ -0,0 +1,22 @@
+package day2;
+
+public class FindIntersection {
+
+	public static void main(String[] args) {
+		int a[]= {3,2,11,46,7};
+		int b[]= {1,2,8,4,9,7};
+		System.out.println("Intersection of two arrays ::");
+		for (int i = 0; i < a.length; i++) {
+			for (int j = 0; j < b.length; j++) {
+				if (a[i]==b[j]) {
+					System.out.println(a[i]);
+					
+				}
+				
+			}
+			
+		}
+
+	}
+
+}
